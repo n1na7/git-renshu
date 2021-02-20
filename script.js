f729ee1ff7da76244a671070db718dcac86ef2be
n1na <info+git@37bit.me>
2021-02-21 02:52:57 +0900

    おみくじが待てるようになりました

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 async function getMikuji() {
+
+    var syori1 = document.getElementById("unsei");
+    syori1.innerHTML = "<p>おみくじを引いてます</p>";
+
     var omikuji = await fetch("https://script.google.com/macros/s/AKfycbwAnFJ18tFZ6_CCK1Pc9rmk2Zczr1hKhy8Oee4-3_ZbKZOvSg/exec");
     var kekka = await omikuji.json()
     console.log(kekka.message)
-    var kuji = document.getElementById('unsei')  
+    var kuji = document.getElementById('unsei') 
     kuji.innerHTML = "<p>あなたの運勢は" + kekka.message +　"です</p>"
 }
