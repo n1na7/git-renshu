5576946db48232c6d716fe95b75df00939f01b23
n1na <info+git@37bit.me>
2021-02-23 01:32:56 +0900

    大吉の処理を追加しました

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ async function getMikuji() {
 
     var omikuji = await fetch("https://script.google.com/macros/s/AKfycbwAnFJ18tFZ6_CCK1Pc9rmk2Zczr1hKhy8Oee4-3_ZbKZOvSg/exec");
     var kekka = await omikuji.json()
+
     console.log(kekka.message)
-    var kuji = document.getElementById('unsei') 
-    kuji.innerHTML = "<p>あなたの運勢は" + kekka.message +　"です</p>"
+
+    var kuji = document.getElementById('unsei')
+
+    if (kekka.message == '大吉') {
+        kuji.innerHTML = "<img class='daikichi' src='./omikuji_daikichi.png' alt='daikichi'>"
+    } else {
+        kuji.innerHTML = "<p>あなたの運勢は" + kekka.message + "です</p>"
+    }
     document.getElementById("implementation").disabled = false;
+
 }
